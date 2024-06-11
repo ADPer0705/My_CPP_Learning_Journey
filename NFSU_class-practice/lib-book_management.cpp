db1cc7f95360f6fd5197fa4e55b7d5234bb478ee
ADPer <adper0705@gmail.com>
2024-06-11 19:01:25 +0530

    made the structure for the library books prgram and added the mechanism for authenticating admin with password
    This commit adds the main funtion, with the menu structured up and the mechanism for authenticating users set up, now on the next step will be to add various functionlaities for the users and admins to the menu respectively and then make the methods to execute those functionlities

--- NFSU_class-practice/lib-book_management.cpp
+++ NFSU_class-practice/lib-book_management.cpp
@@ -1,5 +1,5 @@
 // This program aims at managing book information for a library 
-// This excercise aims at practicing single inheritence in C++
+// This exercise aims at practicing single inheritance in C++
 
 #include<iostream>
 #include<iomanip>
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+// Class to represent a book
 class book
 {
     // Member variables to store the book's title and author
@@ -18,6 +19,7 @@ class book
         // Setter function to set the book's title and author
         void set_details(string title, string author)
         {
+            // Set the title and author of the book
             this->title = title;
             this->author = author;
         }
@@ -25,6 +27,7 @@ class book
         // Getter function to retrieve the book's title and author
         void get_details(string& title, string& author)
         {
+            // Retrieve the title and author of the book
             title = this->title;
             author = this->author;
         }
@@ -32,12 +35,15 @@ class book
         // Function to display the book's information
         void displayBookInfo()
         {
+            // Display the book's title
             cout << setw(10) << right << "Title : " << title << endl;
+            // Display the book's author
             cout << setw(10) << right << "Author : " << author << endl;
         }
 };
 
-class library : private book
+// Class to represent a library, inherits from the book class
+class library : public book
 {
     private : 
         string libId;
@@ -46,7 +52,7 @@ class library : private book
         // Default constructor to initialize the library ID
         library()
         {
-            
+            // Initialize the library ID
         }
 
         // Function to display the book's information with the library ID
@@ -60,8 +66,84 @@ class library : private book
         }
 };
 
+// Array to store 100 library objects
+library lib_books[100];
+
 int main()
 {
-    // Main function to test the program
+    // Initialize all library objects with default title and author
+    for ( int i = 0; i < 100; i ++)
+    {
+        lib_books[i].set_details("N.A.", "N.A.");
+    }
+
+    int ctr, adm_ctr, usr_ctr; 
+    int choice;
+    int adm;
+    string adminPasswd = "aVERYstrongPASSWORD", buffPasswd;
+
+    // Main loop to handle user input
+    while(ctr != 0)
+    {
+        system("clear");
+        cout << "LIBRARY MANAGEMENT SYSTEM" << endl;
+        cout << "Choose your administrative status : " << endl;
+        cout << "1. Administrator" << endl;
+        cout << "2. User" << endl;
+        cout << "0. Exit" << endl;
+
+        // Get user input
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1 :
+                system("clear");
+                // Administrator login
+                for ( int i = 0; i < 3; i ++)
+                {
+                    cout << "Enter your admin ID : ";
+                    cin >> buffPasswd;
+
+                    if (adminPasswd != buffPasswd)
+                    {
+                        system("clear");
+                        cout << "Incorrect admin ID. Please try again." << endl;
+                    }
+
+                    if (adminPasswd == buffPasswd)
+                    {
+                        adm = 1;
+                        break;
+                    }
+
+                    // Administrator menu
+                    while(adm != 0)
+                    {
+                        /* code for admins */
+                    }
+                }
+                break;
+
+            case 2 :
+                system("clear");
+
+                // User menu
+                switch(usr_ctr)
+                {
+                    /* code for users */
+                }
+                break;
+
+            case 0 :
+                system("clear");
+                cout << "Exiting..." << endl;
+                ctr = 0;    
+                break;
+                
+        }
+
+    }
+
     return 0;
 }
